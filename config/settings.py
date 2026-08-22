ba1c3259119997e5be8b5f3b03911a71c1d3f48f
Marcos Araujo <marcos.f.araujo2003@gmail.com>
2026-08-21 23:51:10 -0300

    Criação da model Usuário e Funcionário

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'usuarios',
+    'funcionarios',
 ]
 
 MIDDLEWARE = [
@@ -103,6 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "usuarios.Usuario"
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.1/topics/i18n/
