7f03913e88f8140b7f61e7058df6d7c93f8a34ca
Marcos Araujo <marcos.f.araujo2003@gmail.com>
2026-09-12 18:59:43 -0300

    Consulta de dados pessoais

--- usuarios/views_lgpd.py
+++ usuarios/views_lgpd.py
@@ -1,10 +1,52 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models_2fa import Configuracao2FA
+
+
+def obter_dados_pessoais(usuario):
+    dados = {
+        'conta': {
+            'email': usuario.email,
+            'data_criacao': usuario.date_joined,
+            'dois_fatores_ativo': False,
+        },
+        'funcionario': None,
+        'jornadas': [],
+    }
+
+    try:
+        dados['conta']['dois_fatores_ativo'] = usuario.config_2fa.ativado
+    except Configuracao2FA.DoesNotExist:
+        pass
+
+    funcionario = usuario.funcionario
+
+    if funcionario:
+        dados['funcionario'] = {
+            'nome': funcionario.nome,
+            'cpf': funcionario.cpf,
+            'telefone': funcionario.telefone,
+            'cargo': funcionario.cargo.nome if funcionario.cargo else None,
+            'setor': funcionario.cargo.setor.nome if funcionario.cargo else None,
+        }
+
+        dados['jornadas'] = [
+            {
+                'dia_semana': jornada.get_dia_semana_display(),
+                'hora_inicio': jornada.hora_inicio,
+                'hora_fim': jornada.hora_fim,
+            }
+            for jornada in funcionario.jornadas.all().order_by('dia_semana')
+        ]
+
+    return dados
 
 
 @login_required
 def consultar_dados(request):
-    return HttpResponse("Consulta de dados pessoais.")
+    dados = obter_dados_pessoais(request.user)
+    return render(request, 'usuarios/consultar_dados.html', {'dados': dados})
 
 
 @login_required
